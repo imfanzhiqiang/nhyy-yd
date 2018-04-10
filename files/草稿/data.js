297aa21c74ea831ba76a1df9384f106492afd949
imfanzhiqiang <imfanzhiqiang@gmail.com>
2018-04-10 16:57:04 +0800

    1804101656
    1804101656

---
+++ files/草稿/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="草稿.html",d="generationDate",e=new Date(1523350559892.15),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="870dc9c8e6284789afb6a5ee12527a6e",q="type",r="Axure:Page",s="name",t="草稿",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="6074a471b2374a10b502b6424c313440",Y="label",Z="friendlyType",ba="图像",bb="imageBox",bc="styleType",bd="visible",be=true,bf="5029184f066b4df5a37c6e2793fc2aa5",bg="size",bh="width",bi=700,bj="height",bk=1117.375,bl="imageOverrides",bm="images",bn="normal~",bo="images/草稿/u0.png",bp="masters",bq="objectPaths",br="6074a471b2374a10b502b6424c313440",bs="scriptId",bt="u0";
+return _creator();
+})());
